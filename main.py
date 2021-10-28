0327aca11952e04888b911870f8e60ffc09a93c0
rtmlsh <56894987+rtmlsh@users.noreply.github.com>
2021-10-28 16:06:23 +0300

    Добавил скачивание картинки

--- main.py
+++ main.py
@@ -1,7 +1,27 @@
 import requests
+import pprint
+import os
+from urllib.parse import urlparse
 
 
-url = 'https://xkcd.com/353/info.0.json'
-response = requests.get(url)
-response.raise_for_status()
-print(response.json())
+def get_comics():
+    url = 'https://xkcd.com/353/info.0.json'
+    response = requests.get(url)
+    response.raise_for_status()
+    return response.json()
+
+
+def fetch_comics(url):
+    response = requests.get(url)
+    path = urlparse(url)
+    image_name = os.path.split(path.path)[-1]
+    with open(image_name, 'wb') as file:
+        file.write(response.content)
+
+
+url = get_comics()['img']
+print(get_comics()['alt'])
+fetch_comics(url)
+
+
+
