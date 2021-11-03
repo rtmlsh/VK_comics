0649d36a6974d62a0b65cd8974ca40d14b89a22f
rtmlsh <56894987+rtmlsh@users.noreply.github.com>
2021-11-03 18:04:07 +0300

    Добавил возможность ввода id клиента, группы и токена

--- main.py
+++ main.py
@@ -35,30 +35,41 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         description='Скрипт интегрируется с API VK и xkcd, '
                     'и публикует комиксы на стене вк-сообщества'
+
     )
+    parser.add_argument(
+        'client_id',
+        help='Укажите client_id',
+    )
+    parser.add_argument(
+        'group_id',
+        help='Укажите group_id',
+    )
+    args = parser.parse_args()
     parser.parse_args()
 
+    get_access_url(args.client_id)
+    token = input('Введите access_token: ')
+    if check_access_url(token):
+        with open('.env', 'a') as file:
+            file.write(f'ACCESS_TOKEN={token}')
+
     load_dotenv()
-    client_id = os.getenv('CLIENT_ID')
     access_token = os.getenv('ACCESS_TOKEN')
-    group_id = os.getenv('GROUP_ID')
 
     url, comment = get_comics()
     image_name = fetch_comics(url)
 
-    # get_access_url(client_id)
-    # check_access_url(access_token)
-
-    upload_url = get_upload_url(access_token, group_id)
+    upload_url = get_upload_url(access_token, args.group_id)
     vk_hash, vk_photo, vk_server = upload_image(upload_url, image_name)
     media_id, owner_id = save_image(
         access_token,
-        group_id,
+        args.group_id,
         vk_server,
         vk_hash,
         vk_photo
     )
-    response = post_comics(access_token, group_id, media_id, owner_id, comment)
+    response = post_comics(access_token, args.group_id, media_id, owner_id, comment)
 
     if response:
         os.remove(image_name)
