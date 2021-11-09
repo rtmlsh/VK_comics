3d8f7bf74a4ccf40fb3288a46089d4153e295b61
rtmlsh <56894987+rtmlsh@users.noreply.github.com>
2021-11-09 10:59:14 +0300

    Добавил модуль получения токена

---
+++ get_token.py
@@ -0,0 +1,48 @@
+import requests
+import argparse
+
+def get_access_url(client_id):
+    vk_url = 'https://oauth.vk.com/authorize'
+    payload = {
+        'client_id': client_id,
+        'scope': 'photos,groups,wall,offline',
+        'response_type': 'token'
+    }
+    response = requests.get(vk_url, params=payload)
+    print(response.url)
+
+
+def check_access_url(access_token):
+    vk_api_url = 'https://api.vk.com/method/groups.get'
+    payload = {
+        'access_token': access_token,
+        'v': 5.131,
+        'extended': 1,
+    }
+    response = requests.get(vk_api_url, params=payload)
+    response.raise_for_status()
+    return response
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description='Скрипт интегрируется с API VK и xkcd, '
+                    'и публикует комиксы на стене вк-сообщества'
+
+    )
+
+    parser.add_argument(
+        'client_id',
+        help='Укажите client_id',
+    )
+    parser.add_argument(
+        'group_id',
+        help='Укажите group_id',
+    )
+    args = parser.parse_args()
+    parser.parse_args()
+
+    get_access_url(args.client_id)
+    token = input('Введите access_token: ')
+    if check_access_url(token):
+        print('Токен работает')
